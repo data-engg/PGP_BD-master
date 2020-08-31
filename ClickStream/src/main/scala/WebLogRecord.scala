3fea8eac14d7ed8cdc96053ae37769242f2907df
anurag <sharma.anurag2k4@gmail.com>
2020-08-31 22:40:17 +0530

    Removing directory after moving to new repository ClickStreamAnalysis

--- ClickStream/src/main/scala/WebLogRecord.scala
+++
@@ -1,9 +0,0 @@
-case class WebLogRecord(ip : String, 
-                        unm : String, 
-                        uid : String, 
-                        ts : String, 
-                        request : String,
-                        response : String, 
-                        resp_byte : String, 
-                        agent : String, 
-                        ref : String)
